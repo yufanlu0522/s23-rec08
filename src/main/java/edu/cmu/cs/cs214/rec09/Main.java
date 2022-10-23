aa585c409ce6de83221e60670313f70c818c779a
casssie-zhang <46862855+casssie-zhang@users.noreply.github.com>
2022-10-23 18:08:44 -0400

    add some comments for methods

--- src/main/java/edu/cmu/cs/cs214/rec09/Main.java
+++ src/main/java/edu/cmu/cs/cs214/rec09/Main.java
@@ -33,9 +33,15 @@ public class Main {
             .POST(HttpRequest.BodyPublishers.ofString(bodyStr))
             .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        // System.out.println(response.body());
     }
 
+    /**
+     * Making web API requests using the synchronous send method
+     * This means that we can only make requests one after another
+     * @throws IOException
+     * @throws InterruptedException
+     */
     private static void runMultipleSynchronous() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder(
                 URI.create(URL_STR))
@@ -45,11 +51,16 @@ public class Main {
         Instant start = Instant.now();
         for (int i = 0; i < NUM_REQUESTS; i++) {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response.body());
         }
         System.out.println("Total time sync (ms): " + Duration.between(start, Instant.now()).toMillis());
     }
 
+    /**
+     * Making one single API requests using the asynchronous sendAsync method
+     * @throws IOException
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
     private static void runSingleAsync() throws IOException, InterruptedException, ExecutionException {
         HttpRequest request = HttpRequest.newBuilder(
                         URI.create(URL_STR))
@@ -62,14 +73,22 @@ public class Main {
         Instant start = Instant.now();
 
         CompletableFuture<HttpResponse<String>> responseFuture = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        // observe when this statement is printed out to the console. 
         responseFuture.thenRun(() -> System.out.println("do other things after finished..."));
         System.out.println("do other things...");
         HttpResponse<String> response = responseFuture.join();
-        System.out.println("The response is:" + response.body());
+        // System.out.println("The response is:" + response.body());
 
-        System.out.println("Total time async (ms): " + Duration.between(start, Instant.now()).toMillis());
+        System.out.println("Total time async for one request (ms): " + Duration.between(start, Instant.now()).toMillis());
     }
 
+    /**
+     * Making web API requests using the asynchronous sendAsync method
+     * But make sure that at most 10 requests are sent to the server at the same time.
+     * 
+     * @throws IOException
+     * @throws InterruptedException
+     */
     private static void runMultipleAsynchronous() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder(
                 URI.create(URL_STR))
@@ -81,12 +100,12 @@ public class Main {
 
         Instant start = Instant.now();
         /**
-        TODO task 2
-        you need to:
-        1. create Semaphore instance, you may need the acquire() and release() method
-        2. create list of CompletableFuture<HttpResponse<String>> to store the responseFuture of each request
-        3. for each request, use thenAccept() to print, thenRun() to release Semaphore
-        4. for each element in the list, use join() to get the HttpResponse<String>
+         * TODO task 2:
+         * you need to:
+         * 1. create Semaphore instance, you may need the acquire() and release() method
+         * 2. create list of CompletableFuture<HttpResponse<String>> to store the responseFuture of each request
+         * 3. for each request, use thenAccept() to print, thenRun() to release Semaphore
+         * 4. for each element in the list, use join() to get the HttpResponse<String>
          */
 
         System.out.println("Total time async (ms): " + Duration.between(start, Instant.now()).toMillis());
@@ -96,8 +115,8 @@ public class Main {
         // Task 1
         runWebAPIRequest();
         // Task 2
-//        runMultipleSynchronous();
-//        runSingleAsync();
+    //    runMultipleSynchronous();
+    //    runSingleAsync();
 //        runMultipleAsynchronous();
         System.exit(0);
     }
